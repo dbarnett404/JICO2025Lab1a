9c509aded099f7fb9acfc0ab2b6230de966e42a0
dbarnett404 <db486@sussex.ac.uk>
2025-01-31 09:42:51 +0000

    Source

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        There are 8 primitive data tyres in Java
+        1. byte
+        2. short
+        3. int
+        4. long
+        5. float
+        6. double
+        7. char
+        8. boolean
+        We tend to use int, double, and boolean the most.
+         */
+        System.out.println("Lab 1a");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number1 = scanner.nextInt();
+        System.out.println("You entered: " + number1);
+
+        System.out.println("Enter a second number: ");
+        int number2 = scanner.nextInt();
+        System.out.println("You entered: " + number1 + " and " + number2);
+        //Perform the basic mathematical operations of +, -, /, * and %.
+        //Output the results of each operation.
+
+        //Cut and past the code above but use doubles instead of integers.
+
+        //If you complete this ask for the next task.
+    }
+}
